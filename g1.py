dd842bae7bf48b3f49795c6c8d5bad5f0c4fae18
Andras Toth <andras.toth4@mail.dcu.ie>
2024-10-07 14:03:10 +0100

    g1 done

--- g1.py
+++ g1.py
@@ -1,13 +1,24 @@
 import numpy as np
 from matplotlib import pyplot as plt
+from plotter import Figure
 
-x, y = np.meshgrid(np.arange(0, 2 * np.pi, .2), np.arange(0, 2 * np.pi, .2))
-vx = np.cos(x)
-vy = np.sin(y)
-plt.figure(figsize=(6, 6), dpi=600)
-plt.gca().set_aspect('equal', adjustable='box')  # Make plot box square
-plt.xlabel('x')
-plt.ylabel('y')
-plt.title('Example of a quiver plot')
-plt.quiver(x, y, vx, vy, pivot='mid', label='$v_x$ = cos($x$), $v_y$ = sin($y$)')
-plt.legend()
+start, end = 0, 2 * np.pi
+dim = 101
+step = 5
+    
+
+
+x, y = np.meshgrid(np.linspace(start, end, dim), np.linspace(start, end, dim))
+u = np.cos(x) * y
+v = np.sin(x) * x
+
+x_sliced = x[::step, ::step]
+y_sliced = y[::step, ::step]
+u_sliced = u[::step, ::step]
+v_sliced = v[::step, ::step]
+
+fig = Figure(figsize=(6, 6), xlim=[start, end], ylim=[start, end], x_label=r"$x$ (arb. units)", y_label=r"$y$ (arb. units)")
+fig.quiver(x_sliced, y_sliced, u_sliced, v_sliced, pivot='mid', label=r"$u=cos(x)*y$, $v=sin(x)*x$")
+fig.legend(loc='upper right')
+
+
